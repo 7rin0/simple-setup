e6a5b02ede00b0a3a6ee4393595ef16a3a16c0c5
Luis SEVERINO <lfs.severino@gmail.com>
2016-09-12 00:39:02 +0200

    implement basic http methods

--- application/src/AppBundle/Controller/UserController.php
+++ application/src/AppBundle/Controller/UserController.php
@@ -31,23 +31,72 @@ class UserController extends FOSRestController
     return $restresult;
   }
 
-    /**
-    * @Rest\Post("/user/")
-    */
-    public function postAction(Request $request)
+  /**
+  * @Rest\Post("/user/")
+  */
+  public function postAction(Request $request)
+  {
+    $newUser = new User;
+    $name = $request->get('name');
+    $role = $request->get('role');
+    if(empty($name) || empty($role))
     {
-      $newUser = new User;
-      $name = $request->get('name');
-      $role = $request->get('role');
-      if(empty($name) || empty($role))
-      {
-       return new View("NULL VALUES ARE NOT ALLOWED", Response::HTTP_NOT_ACCEPTABLE);
-      }
-      $newUser->setName($name);
-      $newUser->setRole($role);
-      $entityManager = $this->getDoctrine()->getManager();
-      $entityManager->persist($newUser);
-      $entityManager->flush();
-      return new View("User Added Successfully", Response::HTTP_OK);
+     return new View("NULL VALUES ARE NOT ALLOWED", Response::HTTP_NOT_ACCEPTABLE);
     }
+    $newUser->setName($name);
+    $newUser->setRole($role);
+    $entityManager = $this->getDoctrine()->getManager();
+    $entityManager->persist($newUser);
+    $entityManager->flush();
+    return new View("User Added Successfully", Response::HTTP_OK);
+  }
+
+  /**
+  * @Rest\Put("/user/{id}")
+  */
+  public function updateAction($id, Request $request)
+  {
+    $data = new User;
+    $name = $request->get('name');
+    $role = $request->get('role');
+    $sn = $this->getDoctrine()->getManager();
+    $user = $this->getDoctrine()->getRepository('AppBundle:User')->find($id);
+    if (empty($user)) {
+     return new View("user not found", Response::HTTP_NOT_FOUND);
+    }
+    elseif(!empty($name) && !empty($role)){
+     $user->setName($name);
+     $user->setRole($role);
+     $sn->flush();
+     return new View("User Updated Successfully", Response::HTTP_OK);
+    }
+    elseif(empty($name) && !empty($role)){
+     $user->setRole($role);
+     $sn->flush();
+     return new View("role Updated Successfully", Response::HTTP_OK);
+    }
+    elseif(!empty($name) && empty($role)){
+    $user->setName($name);
+    $sn->flush();
+    return new View("User Name Updated Successfully", Response::HTTP_OK);
+    }
+    else return new View("User name or role cannot be empty", Response::HTTP_NOT_ACCEPTABLE);
+  }
+
+  /**
+   * @Rest\Delete("/user/{id}")
+   */
+   public function deleteAction($id)
+   {
+    $data = new User;
+    $sn = $this->getDoctrine()->getManager();
+    $user = $this->getDoctrine()->getRepository('AppBundle:User')->find($id);
+    if (empty($user)) {
+      return new View("user not found", Response::HTTP_NOT_FOUND);
+    } else {
+      $sn->remove($user);
+      $sn->flush();
+    }
+    return new View("deleted successfully", Response::HTTP_OK);
+   }
 }
