8e1ca2b4485abd233147df9e847e19df3a37d593
Luis SEVERINO <lfs.severino@gmail.com>
2016-09-22 01:51:53 +0200

    add debug and traceback php tool kint

--- container/phalcon3/app/controllers/IndexController.php
+++ container/phalcon3/app/controllers/IndexController.php
@@ -9,8 +9,20 @@ class IndexController extends Controller
 {
     public function indexAction()
     {
+      // Simple test, to be removed soon ...
+      $this->debugAction();
+
       $twig = $this->getDi()->get('twig');
       $template = $twig->loadTemplate('index.html.twig');
       echo $template->render(array('hello_world'=>'Hello world! Twig variable.'));
+    }
+
+    /**
+     * Not a route yet ...
+     */
+    public function debugAction()
+    {
+      // Simple test, to be removed soon ...
+      Kint::dump($GLOBALS, $_SERVER);
     }
 }
